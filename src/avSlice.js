fcf264bcfa3df348bc74989de4647be782814b61
Jarad Inniss <jjinniss02@hotmail.com>
2026-01-01 04:18:25 -0500

    Add code for Add-ons section

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,51 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+        img: "https://pixabay.com/images/download/business-20031_640.jpg",
+        name: "Projector",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/71hwH97fbcL._AC_SY879_.jpg",
+        name: "Loud Speaker",
+        cost: 80,
+        quantity: 0,
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/61A8Yf-BM-L._AC_SX679_.jpg",
+        name: "Microphone",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/61yCqEYfkeL._AC_SL1500_.jpg",
+        name: "Whiteboard",
+        cost: 50,
+        quantity: 0,
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/61OvOswuowL._AC_SL1500_.jpg",
+        name: "Signage",
+        cost: 25,
+        quantity: 0,
+    },
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+        const item = state[action.payload];
+        if (item) {
+            item.quantity += 1;
+        }
     },
     decrementAvQuantity: (state, action) => {
-     
+        const item = state[action.payload];
+        if (item && item.quantity > 0) {
+            item.quantity -= 1;
+        }
     },
   },
 });
--- src/venueSlice.js
+++ src/venueSlice.js
@@ -5,44 +5,49 @@ export const venueSlice = createSlice({
   name: "venue",
   initialState: [
     {
-      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
-      name: "Conference Room (Capacity:15)",
-      cost: 3500,
-      quantity: 0,
-    },
-    {
-      img: "https://pixabay.com/images/download/event-venue-1597531_640.jpg",
-      name: "Auditorium Hall (Capacity:200)",
-      cost: 5500,
-      quantity: 0,
+        img: "https://pixabay.com/images/download/event-venue-1597531_640.jpg",
+        name: "Auditorium Hall",
+        capacity: 200,
+        cost: 5500,
+        quantity: 0,
     },
     {
       img: "https://pixabay.com/images/download/convention-center-3908238_640.jpg",
-      name: "Presentation Room (Capacity:50)",
+      name: "Presentation Room",
+      capacity: 50,
       cost: 700,
       quantity: 0,
     },
     {
+        img: "https://www.wework.com/ideas/wp-content/uploads/sites/4/2021/08/20201008-199WaterSt-2_fb.jpg?fit=1200%2C675",
+        name: "Conference Room",
+        capacity: 15,
+        cost: 3500,
+        quantity: 0,
+    },
+    {
       img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
-      name: "Large Meeting Room (Capacity:10)",
+      name: "Large Meeting Room",
+      capacity: 10,
       cost: 900,
       quantity: 0,
     },
     {
       img: "https://pixabay.com/images/download/laptops-593296_640.jpg",
-      name: "Small Meeting Room (Capacity:5)",
+      name: "Small Meeting Room",
+      capacity: 5,
       cost: 1100,
       quantity: 0,
     },
-  
   ],
   reducers: {
    
     incrementQuantity: (state, action) => {
       const { payload: index } = action;
       if (state[index]) {
-        if (state[index].name === " Auditorium Hall (Capacity:200)" && state[index].quantity >= 3) {
-          return;        }
+        if (state[index].name === "Auditorium Hall" && state[index].quantity >= 3) {
+          return;        
+        }
         state[index].quantity++;
       }
     },
